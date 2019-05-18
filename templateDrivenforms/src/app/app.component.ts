c853532036efbe292e7d255aca9e279a858c6ff5
singhtalvinder <talvindersingh.ca@gmail.com>
2019-05-18 13:44:51 -0400

    Added form validation and submit button check.

--- templateDrivenforms/src/app/app.component.ts
+++ templateDrivenforms/src/app/app.component.ts
@@ -9,8 +9,19 @@ import { User } from './user';
 export class AppComponent {
   title = 'templateDrivenforms';
 
+  // topic validated.
+  topicHasError = true;
   // Initial user object , can be used to prefill the form when visible.
-  userModel = new User('Kim','kim@test.com', 1234567890, '', 'evening', true);
+  userModel = new User('Ming','kim@test.com', 1234567890, 'default', 'evening', true);
 
   topics = ['Angular', 'C++', 'Java'];
+
+  // Validate topic for selecting atleast one option. Should not be the "Please select..."
+  validateTopic(value) {
+    if(value ==='default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
 }
